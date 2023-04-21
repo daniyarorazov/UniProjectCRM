d4d61ca9d2bb2e8dd71cb316a8fd1ac998184db1
Daniyar Orazov <orazov.video@gmail.com>
2023-04-21 06:45:02 +0200

    Added validation for inputs

--- UniProjectCRM/Program.cs
+++ UniProjectCRM/Program.cs
@@ -1,30 +1,84 @@
 ﻿
 using System;
-
+using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Xml.Linq;
 
 namespace UniProjectCRM
 {
 
     class MenuActions
     {
+        public string IfIsNullValue(string value, string nameField)
+        {
+            while (string.IsNullOrEmpty(value))
+            {
+                Console.WriteLine("Invalid input. Please enter a non-empty [{0}]:", nameField);
+                value = Console.ReadLine();
+                
+
+            }
+            return value;
+        }
+
+        public string DateValidation(string value)
+        {
+            DateTime date;
+
+            while (!DateTime.TryParseExact(value, "dd.MM.yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out date) || date > DateTime.Today)
+            {
+                if (date > DateTime.Today)
+                {
+                    Console.WriteLine("Invalid input. The date cannot be later than today's date.");
+                    value = Console.ReadLine();
+                } else
+                {
+                    Console.WriteLine("Invalid input. Please enter a date in the format dd.MM.yyyy:");
+                    value = Console.ReadLine();
+                }
+               
+            }
+            return value;
+        }
         public void AddClient()
         {
             StreamWriter sw = new StreamWriter("./clients.csv", true);
-
-            while (true)
+            bool state = true;
+            while (state)
             {
                 Console.WriteLine("Name of client");
                 string name = Console.ReadLine();
+                name = IfIsNullValue(name, "Name of client");
+
                 Console.WriteLine("Surname of client");
                 string surname = Console.ReadLine();
-                Console.WriteLine("Date of Birth client (24/02/2022)");
+                surname = IfIsNullValue(surname, "Surname of client");
+
+                Console.WriteLine("Date of Birth client (example: 24.02.2022)");
                 string dateBirth = Console.ReadLine();
+                dateBirth = IfIsNullValue(dateBirth, "Date of Birth client (example: 24.02.2022)");
+                dateBirth = DateValidation(dateBirth);
+
                 Console.WriteLine("Sum of all purchases ($)");
                 string SumAllPurchases = Console.ReadLine();
-                Console.WriteLine("Last contact with him (24/02/2022)");
+                SumAllPurchases = IfIsNullValue(SumAllPurchases, "Sum of all purchases ($)");
+
+                decimal sum;
+
+                while (!decimal.TryParse(SumAllPurchases, out sum))
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid number for the sum of all purchases:");
+                    SumAllPurchases = Console.ReadLine();
+                }
+
+
+
+                Console.WriteLine("Last contact with him (example: 24.02.2022)");
                 string lastActivityWithClient = Console.ReadLine();
+                lastActivityWithClient = IfIsNullValue(lastActivityWithClient, "Last contact with him (example: 24.02.2022)");
+                lastActivityWithClient = DateValidation(lastActivityWithClient);
+
                 string output = string.Format("{0}, {1}, {2}, {3}$, {4}", name, surname, dateBirth, SumAllPurchases, lastActivityWithClient);
                 sw.WriteLine(output);
 
@@ -33,8 +87,33 @@ namespace UniProjectCRM
                 string answerForAddingAnother = Console.ReadLine();
                 if (answerForAddingAnother == "2")
                 {
+                    state = false;
                     break;
                 }
+                if (answerForAddingAnother == "1") {
+                    state = true;
+                    Console.Clear();
+                }
+                while (answerForAddingAnother != "1" && answerForAddingAnother != "2")
+                {
+                    Console.WriteLine("Do you want to add another user? \n 1. Yes \n 2. No");
+                    answerForAddingAnother = Console.ReadLine();
+                    if (answerForAddingAnother == "2")
+                    {
+                        state = false;
+                        break;
+                    }
+                    if (answerForAddingAnother == "1")
+                    {
+                        state = true;
+                        break;
+                    }
+                }
+                if (!state)
+                {
+                    break;
+                }
+
             }
             sw.Close();
         }
@@ -93,7 +172,16 @@ namespace UniProjectCRM
             ShowAllClients();
             string[] lines = File.ReadAllLines("./clients.csv");
             Console.Write("Choose the number of the client to edit: ");
-            int index = int.Parse(Console.ReadLine()) - 1;
+            string indexNum = Console.ReadLine();
+            decimal num;
+
+            while (!decimal.TryParse(indexNum, out num))
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number");
+                indexNum = Console.ReadLine();
+            }
+
+            int index = int.Parse(indexNum) - 1;
             if (index < 0 || index >= lines.Length)
             {
                 Console.WriteLine("Invalid client number.");
@@ -104,12 +192,27 @@ namespace UniProjectCRM
             string[] chosenFields = lines[index].Split(',');
             Console.Write("Enter new date of birth: ");
             string newDob = Console.ReadLine();
+            newDob = IfIsNullValue(newDob, "Date of birth");
+            newDob = DateValidation(newDob);
+
             Console.Write("Enter new sum of payments: ");
             string newPayments = Console.ReadLine();
+            newPayments = IfIsNullValue(newPayments, "Date of birth");
+            decimal sum;
+
+            while (!decimal.TryParse(newPayments, out sum))
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number for the sum of all purchases:");
+                newPayments = Console.ReadLine();
+            }
+
             Console.Write("Enter new last interaction date: ");
             string newInteraction = Console.ReadLine();
+            newInteraction = IfIsNullValue(newInteraction, "Last interaction");
+            newInteraction = DateValidation(newInteraction);
+
             chosenFields[2] = newDob;
-            chosenFields[3] = newPayments;
+            chosenFields[3] = (newPayments + "$");
             chosenFields[4] = newInteraction;
             lines[index] = string.Join(",", chosenFields);
 
@@ -119,24 +222,63 @@ namespace UniProjectCRM
         }
         public void DeleteClient()
         {
-            ShowAllClients();
             string[] lines = File.ReadAllLines("./clients.csv");
-            Console.Write("Enter the ID of the client you want to delete: ");
-            int id = int.Parse(Console.ReadLine());
+            bool state = true;
 
-            // Remove the selected row from the list of rows
-            lines = lines.Where((line, index) => index != id - 1).ToArray();
-
-            // Write the updated list of rows back to the CSV file
-            using (var writer = new StreamWriter("clients.csv"))
+            while (state)
             {
-                foreach (var row in lines)
+                ShowAllClients();
+                Console.Write("Enter the ID of the client you want to delete: ");
+                int id = int.Parse(Console.ReadLine());
+
+                // Remove the selected row from the list of rows
+                lines = lines.Where((line, index) => index != id - 1).ToArray();
+
+                // Write the updated list of rows back to the CSV file
+                using (var writer = new StreamWriter("clients.csv"))
                 {
-                    writer.WriteLine(row);
+                    foreach (var row in lines)
+                    {
+                        writer.WriteLine(row);
+                    }
+                }
+
+                Console.WriteLine("Client with ID {0} has been deleted.", id);
+
+                Console.WriteLine("Do you want to delete another user? \n 1. Yes \n 2. No");
+                string answerForAddingAnother = Console.ReadLine();
+                if (answerForAddingAnother == "2")
+                {
+                    state = false;
+                    break;
+                }
+                if (answerForAddingAnother == "1")
+                {
+                    state = true;
+                    Console.Clear();
+                }
+                while (answerForAddingAnother != "1" && answerForAddingAnother != "2")
+                {
+                    Console.WriteLine("Do you want to delete another user? \n 1. Yes \n 2. No");
+                    answerForAddingAnother = Console.ReadLine();
+                    if (answerForAddingAnother == "2")
+                    {
+                        state = false;
+                        break;
+                    }
+                    if (answerForAddingAnother == "1")
+                    {
+                        state = true;
+                        Console.Clear();
+
+                        break;
+                    }
+                }
+                if (!state)
+                {
+                    break;
                 }
             }
-
-            Console.WriteLine("Client with ID {0} has been deleted.", id);
         }
     }
 
@@ -156,7 +298,15 @@ namespace UniProjectCRM
                 "4. Delete client \n " +
                 "5. Exit ");
                 string option = Console.ReadLine();
-                int num = Convert.ToInt32(option);
+                int num;
+
+                if (!int.TryParse(option, out num) || num > 5)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Invalid input. Please enter a number between 1 and 5.");
+                    continue;
+                    
+                }
                 if (num == 5)
                 {
                     break;
@@ -164,26 +314,27 @@ namespace UniProjectCRM
                 switch (num)
                 {
                     case 1:
+                        Console.Clear();
                         menuActions.AddClient();
                         Console.Clear();
                         break;
-
                     case 2:
+                        Console.Clear();
                         menuActions.ShowAllClients();
                         Console.WriteLine("Enter any value for continue ->");
                         Console.ReadLine();
                         Console.Clear();
                         break;
                     case 3:
+                        Console.Clear();
                         menuActions.EditDataClient();
                         Console.WriteLine("Enter any value for continue ->");
                         Console.ReadLine();
                         Console.Clear();
                         break;
                     case 4:
+                        Console.Clear();
                         menuActions.DeleteClient();
-                        Console.WriteLine("Enter any value for continue ->");
-                        Console.ReadLine();
                         Console.Clear();
                         break;
                     default:
